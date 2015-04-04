69182471b348fc621c254469224340fead2bfe1d
Raphaël M. <Galixte@users.noreply.github.com>
2015-04-04 07:29:10 +0200

    Update of FRENCH translation for External Links extension
    - Update of FRENCH translation for exlinks.php file
    - Update of FRENCH translation for info_acp_exlinks.php file

--- language/fr/exlinks.php
+++ language/fr/exlinks.php
@@ -1,7 +1,8 @@
 <?php
 /**
 *
-* External Links [English]
+* External Links [French]
+* French translation by Galixte (http://www.galixte.com)
 *
 * @package info_acp_exlinks.php
 * @copyright (c) 2014 Anvar http://bb3.mobi
@@ -9,17 +10,41 @@
 *
 */
 
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
 if (empty($lang) || !is_array($lang))
 {
 	$lang = array();
 }
 
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge($lang, array(
-	'HIDE_LINKS_GUESTS'		=> 'To view links you must be logged in.',
-	'HIDE_LINKS_MIN_POSTS'	=> 'To view links you must first <strong>%s</strong> posts.',
-	'EXTERNAL_REDIRECT'		=> 'Transition to an external link',
-	'EXTERNAL_LINK_EXACT'	=> 'The exact link is',
-	'EXTERNAL_MESSAGE_TEXT'	=> 'You have chosen to visit an external link at: <span class="error">%s</span><br /><br />Are you sure you wish to proceed?<br />If the link is suspicious, please let the Admin or Moderator team know.',
-	'URL_LINK'				=> '%sGo to the link%s',
-	'CLOSE_PAGE'			=> '%sClose this page%s',
+	'HIDE_LINKS_GUESTS'		=> 'Vous devez être enregistré et connecté pour pouvoir afficher les liens.',
+	'HIDE_LINKS_MIN_POSTS'	=> 'Vous devez avoir posté <strong>%s</strong> messages pour pouvoir afficher les liens.',
+	'EXTERNAL_REDIRECT'		=> 'Vous allez être redirigé(e) vers un lien externe',
+	'EXTERNAL_LINK_EXACT'	=> 'Le lien exact est',
+	'EXTERNAL_MESSAGE_TEXT'	=> 'Vous avez choisi de vous rendre sur un lien externe au forum : <span class="error">%s</span><br /><br />Êtes-vous sûr(e) de vouloir continuer ?<br />Si le lien est suspecté, veuillez le signaler à l’un des membres de l’équipe forum.',
+	'URL_LINK'				=> ' Se rendre sur le lien',
+	'CLOSE_PAGE'			=> 'seconde(s) avant la redirection - Fermer cette page',
 ));
