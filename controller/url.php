6a664b1c7af9c8087869f45b807291d0fc8e92dd
unknown <C:\Program Files\The Bat!\Mail>
2016-04-05 20:58:13 +0600

    Incorrect links - disable page

--- controller/url.php
+++ controller/url.php
@@ -37,36 +37,35 @@ class url
 
 		$redirect_url = html_entity_decode($location, ENT_QUOTES);
 
-		if (preg_match('#(^|[\n\t (>.])(' . get_preg_expression('url_inline') . ')#iu', $redirect_url))
+		if (!preg_match('#(^|[\n\t (>.])(' . get_preg_expression('url_inline') . ')#iu', $redirect_url))
 		{
-			$s_link_valid = true;
-
-			if (!preg_match('#.#u', $redirect_url))
-			{
-				$redirect_url = iconv('Windows-1251', 'UTF-8', $redirect_url);
-			}
-
-			$redirect_url = $this->convert->encode_uri($redirect_url);
-
-			if (!$this->config['external_link_redirect'])
-			{
-				redirect($redirect_url, false, true);
-			}
-
-			// Time redirect
-			meta_refresh($this->config['external_link_redirect'], $redirect_url, true);
-			//header('Refresh: ' . $this->config['external_link_redirect'] . '; url=' . $redirect_url);
-
-			$parse = parse_url($location);
-
-			$redirect_url = utf8_basename($parse['host']);
+			trigger_error('INSECURE_REDIRECT');
 		}
+
+		if (!preg_match('#.#u', $redirect_url))
+		{
+			$redirect_url = iconv('Windows-1251', 'UTF-8', $redirect_url);
+		}
+
+		$redirect_url = $this->convert->encode_uri($redirect_url);
+
+		if (!$this->config['external_link_redirect'])
+		{
+			redirect($redirect_url, false, true);
+		}
+
+		// Time redirect
+		meta_refresh($this->config['external_link_redirect'], $redirect_url, true);
+		//header('Refresh: ' . $this->config['external_link_redirect'] . '; url=' . $redirect_url);
+
+		$parse = parse_url($location);
+
+		$redirect_url = utf8_basename($parse['host']);
 
 		$this->template->assign_vars(array(
 			'EXTERNAL_MSG'	=> sprintf($this->user->lang['EXTERNAL_MESSAGE_TEXT'], $redirect_url),
 			'EXTERNAL_RED'	=> $this->config['external_link_redirect'],
 			'EXTERNAL_URL'	=> $location,
-			'S_LINK_VALID'	=> (isset($s_link_valid) ? true : false),
 			)
 		);
 
